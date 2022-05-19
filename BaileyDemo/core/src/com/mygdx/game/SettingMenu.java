a93fa965a7ffde8d133288d87abc8dcce073095a
Raniyafa <88967704+Raniyafa@users.noreply.github.com>
2022-05-19 23:35:17 +1200

    Added buttons to sound menu to control background music on/off and sound effects.

--- BaileyDemo/core/src/com/mygdx/game/SettingMenu.java
+++ BaileyDemo/core/src/com/mygdx/game/SettingMenu.java
@@ -1,9 +1,11 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -15,10 +17,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.github.czyzby.websocket.WebSocket;
@@ -31,8 +33,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.Preferences;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
 
-public class SettingMenu extends ScreenAdapter{
+public class SettingMenu extends ScreenAdapter {
 
     private MultipleScenes game;
     private Stage stage;
@@ -48,29 +53,42 @@ public class SettingMenu extends ScreenAdapter{
     private TextureRegion buttonUp;
     private TextureRegion buttonDown;
     private Texture tex2;
+    private Texture tex3;
+    private Texture tex4;
+    private Texture tex5;
+    private Texture tex6;
     private ImageButton button;
 
-    private Slider slider;
+
 
     public SettingMenu(MultipleScenes game) {
         this.game = game;
+
+
     }
 
     @Override
-    public void show(){
+    public void show() {
 
         stage = new Stage(new ScreenViewport());
         skin = new Skin(Gdx.files.internal("plain-james/skin/plain-james-ui.json"));
 
 
-        slider = new Slider(0, 100, 1, false, skin);
-        slider.setValue(1);
-        Container<Slider> sliderContainer = new Container<Slider>(slider);
-        sliderContainer.setTransform(true);
 
-        sliderContainer.setOrigin(slider.getWidth()/2,slider.getHeight()/2);
-        sliderContainer.setSize(slider.getWidth(),slider.getHeight());
-        sliderContainer.setScale(2);
+
+
+
+
+
+
+    //slider = new Slider(0, 100, 1, false, skin);
+        ////slider.setValue(1);
+        //Container<Slider> sliderContainer = new Container<Slider>(slider);
+        //sliderContainer.setTransform(true);
+
+        ////sliderContainer.setOrigin(slider.getWidth()/2,slider.getHeight()/2);
+       // sliderContainer.setSize(slider.getWidth(),slider.getHeight());
+       // sliderContainer.setScale(2);
 
 //        slider.setSize(30, 100);
 //        slider.setPosition(100, 100);
@@ -80,7 +98,7 @@ public class SettingMenu extends ScreenAdapter{
 //
 //            }
 //        });
-        stage.addActor(slider);
+        //stage.addActor(slider);
 
         tex = new Texture(Gdx.files.internal("image/setting.png"));
         region = new TextureRegion(tex,0,0,750,1334);
@@ -115,7 +133,7 @@ public class SettingMenu extends ScreenAdapter{
         up = new TextureRegionDrawable(buttonUp);
         down = new TextureRegionDrawable(buttonDown);
         button = new ImageButton(up,down);
-        button.setPosition(Gdx.graphics.getWidth() / 2 + 100,Gdx.graphics.getHeight()/2 + 195);
+        button.setPosition(Gdx.graphics.getWidth() / 2 + -20,Gdx.graphics.getHeight()/2 + -220);
         button.setSize(30,30);
         stage.addActor(button);
         button.addListener(new ClickListener() {
@@ -125,6 +143,82 @@ public class SettingMenu extends ScreenAdapter{
                 game.setScreen(new gameRuleScreen(game));
             }
         });
+
+        tex3 = new Texture(Gdx.files.internal("button/OnButton.png"));
+        TextureRegion[][] temp_4 = TextureRegion.split(tex3,200,140);
+        buttonUp = temp_4[0][0];
+        buttonDown = temp_4[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        button = new ImageButton(up,down);
+        button.setPosition(Gdx.graphics.getWidth() / 2 + -120,Gdx.graphics.getHeight()/2 + 5);
+        button.setSize(100,70);
+        stage.addActor(button);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.background.play();
+                SoundManager.button.play();
+
+            }
+        });
+
+        tex4 = new Texture(Gdx.files.internal("button/OffButton.png"));
+        TextureRegion[][] temp_5 = TextureRegion.split(tex4,200,140);
+        buttonUp = temp_5[0][0];
+        buttonDown = temp_5[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        button = new ImageButton(up,down);
+        button.setPosition(Gdx.graphics.getWidth() / 2 + 10,Gdx.graphics.getHeight()/2 + 5);
+        button.setSize(100,70);
+        stage.addActor(button);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+                SoundManager.background.pause();
+                SoundManager.button.play();
+            }
+        });
+
+        tex5 = new Texture(Gdx.files.internal("button/OnButton1.png"));
+        TextureRegion[][] temp_6 = TextureRegion.split(tex5,200,140);
+        buttonUp = temp_6[0][0];
+        buttonDown = temp_6[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        button = new ImageButton(up,down);
+        button.setPosition(Gdx.graphics.getWidth() / 2 + -120,Gdx.graphics.getHeight()/2 + -150);
+        button.setSize(100,70);
+        stage.addActor(button);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.resume();
+
+            }
+        });
+
+        tex6 = new Texture(Gdx.files.internal("button/OffButton1.png"));
+        TextureRegion[][] temp_7 = TextureRegion.split(tex6,200,140);
+        buttonUp = temp_7[0][0];
+        buttonDown = temp_7[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        button = new ImageButton(up,down);
+        button.setPosition(Gdx.graphics.getWidth() / 2 + 10,Gdx.graphics.getHeight()/2 + -150);
+        button.setSize(100,70);
+        stage.addActor(button);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+               SoundManager.button.dispose();
+            }
+        });
+
 
         Gdx.input.setInputProcessor(stage);
     }
