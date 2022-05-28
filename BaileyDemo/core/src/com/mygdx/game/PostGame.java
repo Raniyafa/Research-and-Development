ea0f92e1f0883ddc440983c51772ad441ef94c0f
TQkillsISAAC <gdn5850@autuni.ac.nz>
2022-05-28 15:18:11 +1200

    Changed the Game Over Scene's UI

--- BaileyDemo/core/src/com/mygdx/game/PostGame.java
+++ BaileyDemo/core/src/com/mygdx/game/PostGame.java
@@ -29,10 +29,10 @@ public class PostGame extends ScreenAdapter {
 
     private MultipleScenes game;
 
-    private TextButton facebookPost;
-    private TextButton twitterPost;
-    private TextButton instagramPost;
-    private TextButton exitToMenu;
+//    private TextButton facebookPost;
+//    private TextButton twitterPost;
+//    private TextButton instagramPost;
+//    private TextButton exitToMenu;
 
     private Texture tex;
     private Image image;
@@ -42,6 +42,7 @@ public class PostGame extends ScreenAdapter {
     private Stage stage;
     private Skin mySkin;
     private BitmapFont font;
+    private BitmapFont smallFont;
     private BitmapFont fontLarge;
     private boolean moveToMain = false;
     private ShapeRenderer shapeRenderer;
@@ -53,6 +54,10 @@ public class PostGame extends ScreenAdapter {
     private TextureRegion buttonDown;
     private Texture tex2;
     private ImageButton button;
+    private ImageButton FaceBook;
+    private ImageButton Twitter;
+    private ImageButton Instagram;
+    private ImageButton Share;
 
     public PostGame(MultipleScenes game, ArrayList<Shape> shapeArray) {
         shapeArr = shapeArray;
@@ -65,6 +70,10 @@ public class PostGame extends ScreenAdapter {
         font = new BitmapFont(Gdx.files.internal("font/dbfont.fnt"),
                 Gdx.files.internal("font/dbfont.png"), false);
         font.setColor(Color.BLACK);
+
+        smallFont = new BitmapFont(Gdx.files.internal("font/dbSmallFont.fnt"),
+                Gdx.files.internal("font/dbSmallFont.png"), false);
+        smallFont.setColor(Color.BLACK);
 
         //Adding Background (new)
         tex = new Texture(Gdx.files.internal("image/gameFinish.png"));
@@ -79,39 +88,98 @@ public class PostGame extends ScreenAdapter {
         float widthSlice = Gdx.graphics.getWidth() / 20;
 
 
-        facebookPost = new TextButton("Facebook", mySkin, "toggle");
-        facebookPost.setBounds(widthSlice, Gdx.graphics.getHeight()/2 - 310, 80, 40);
-        facebookPost.getLabel().setFontScale(0.6f, 0.6f);
-        facebookPost.addListener(new InputListener(){
+//        facebookPost = new TextButton("Facebook", mySkin, "toggle");
+//        facebookPost.setBounds(widthSlice, Gdx.graphics.getHeight()/2 - 310, 80, 40);
+//        facebookPost.getLabel().setFontScale(0.6f, 0.6f);
+//        facebookPost.addListener(new InputListener(){
+//
+//            @Override
+//            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//
+//            }
+//            @Override
+//            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+//                Gdx.net.openURI("https://www.facebook.com/sharer/sharer.php?u=https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-70x70.png&quote=TEST DRAWING FROM DRAW BUDDY TEST");
+//                return true;
+//            }
+//        });
+//        facebookPost.setVisible(false);
+//        stage.addActor(facebookPost);
 
+        tex2 = new Texture(Gdx.files.internal("button/FaceBookButton.png"));
+        TextureRegion[][] temp_f = TextureRegion.split(tex2,512,512);
+        buttonUp = temp_f[0][0];
+        buttonDown = temp_f[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        FaceBook = new ImageButton(up,down);
+        FaceBook.setPosition(Gdx.graphics.getWidth()/2 + 10,Gdx.graphics.getHeight() / 2 - 210);
+        FaceBook.setSize(45,45);
+        stage.addActor(FaceBook);
+        FaceBook.setVisible(false);
+        FaceBook.addListener(new ClickListener() {
             @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.play();
                 Gdx.net.openURI("https://www.facebook.com/sharer/sharer.php?u=https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-70x70.png&quote=TEST DRAWING FROM DRAW BUDDY TEST");
-                return true;
             }
         });
-        stage.addActor(facebookPost);
 
-        twitterPost = new TextButton("Twitter", mySkin, "toggle");
-        twitterPost.setBounds(widthSlice * 6, Gdx.graphics.getHeight()/2 - 310, 80, 40);
-        twitterPost.getLabel().setFontScale(0.6f, 0.6f);
-        twitterPost.addListener(new InputListener(){
+//        twitterPost = new TextButton("Twitter", mySkin, "toggle");
+//        twitterPost.setBounds(widthSlice * 6, Gdx.graphics.getHeight()/2 - 310, 80, 40);
+//        twitterPost.getLabel().setFontScale(0.6f, 0.6f);
+//        twitterPost.addListener(new InputListener(){
+//
+//            @Override
+//            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//
+//            }
+//            @Override
+//            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+//                Gdx.net.openURI("https://twitter.com/share?ref_src=https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-70x70.png");
+//                return true;
+//            }
+//        });
+//        twitterPost.setVisible(false);
+//        stage.addActor(twitterPost);
 
+        tex2 = new Texture(Gdx.files.internal("button/TwitterButton.png"));
+        TextureRegion[][] temp_t = TextureRegion.split(tex2,512,512);
+        buttonUp = temp_t[0][0];
+        buttonDown = temp_t[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        Twitter = new ImageButton(up,down);
+        Twitter.setPosition(Gdx.graphics.getWidth()/2 + 65,Gdx.graphics.getHeight() / 2 - 210);
+        Twitter.setSize(45,45);
+        stage.addActor(Twitter);
+        Twitter.setVisible(false);
+        Twitter.addListener(new ClickListener() {
             @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.play();
                 Gdx.net.openURI("https://twitter.com/share?ref_src=https://static-cdn.jtvnw.net/user-default-pictures-uv/215b7342-def9-11e9-9a66-784f43822e80-profile_image-70x70.png");
-                return true;
             }
         });
-        stage.addActor(twitterPost);
+
+        tex2 = new Texture(Gdx.files.internal("button/Instagram.png"));
+        TextureRegion[][] temp_i = TextureRegion.split(tex2,512,512);
+        buttonUp = temp_i[0][0];
+        buttonDown = temp_i[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        Instagram = new ImageButton(up,down);
+        Instagram.setPosition(Gdx.graphics.getWidth()/2 + 120,Gdx.graphics.getHeight() / 2 - 210);
+        Instagram.setSize(45,45);
+        stage.addActor(Instagram);
+        Instagram.setVisible(false);
+        Instagram.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.play();
+                Gdx.net.openURI("https://www.example.com");
+            }
+        });
 
         //back to lobby button
         tex2 = new Texture(Gdx.files.internal("button/BackToLobbyButton.png"));
@@ -121,14 +189,33 @@ public class PostGame extends ScreenAdapter {
         up = new TextureRegionDrawable(buttonUp);
         down = new TextureRegionDrawable(buttonDown);
         button = new ImageButton(up,down);
-        button.setPosition(Gdx.graphics.getWidth()/2 - 100,Gdx.graphics.getHeight() / 2 - 280);
-        button.setSize(200,100);
+        button.setPosition(Gdx.graphics.getWidth()/2 + 10,Gdx.graphics.getHeight() / 2 - 280);
+        button.setSize(133,66);
         stage.addActor(button);
         button.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 SoundManager.button.play();
                 game.setScreen(new HomeScreen(game));
+            }
+        });
+
+        //Save button
+        tex2 = new Texture(Gdx.files.internal("button/SaveButton.png"));
+        TextureRegion[][] temp_2 = TextureRegion.split(tex2,220,400);
+        buttonUp = temp_2[0][0];
+        buttonDown = temp_2[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        button = new ImageButton(up,down);
+        button.setPosition(Gdx.graphics.getWidth()/2 - 75,Gdx.graphics.getHeight() / 2 - 280);
+        button.setSize(73,133);
+        stage.addActor(button);
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.play();
+                // code here ~~
             }
         });
 
@@ -149,25 +236,53 @@ public class PostGame extends ScreenAdapter {
 //            }
 //        });
 //        stage.addActor(exitToMenu);
+//
+//        instagramPost = new TextButton("Instagram", mySkin, "toggle");
+//        instagramPost.setBounds(widthSlice * 11, Gdx.graphics.getHeight()/2 - 310, 80, 40);
+//        instagramPost.getLabel().setFontScale(0.6f, 0.6f);
+//        instagramPost.addListener(new InputListener(){
+//
+//            @Override
+//            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//
+//            }
+//            @Override
+//            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+//                Gdx.net.openURI("https://www.example.com");
+//                return true;
+//            }
+//        });
+//        instagramPost.setVisible(false);
+//        stage.addActor(instagramPost);
 
-        instagramPost = new TextButton("Instagram", mySkin, "toggle");
-        instagramPost.setBounds(widthSlice * 11, Gdx.graphics.getHeight()/2 - 310, 80, 40);
-        instagramPost.getLabel().setFontScale(0.6f, 0.6f);
-        instagramPost.addListener(new InputListener(){
-
+        //share button
+        tex2 = new Texture(Gdx.files.internal("button/ShareButton.png"));
+        TextureRegion[][] temp_1 = TextureRegion.split(tex2,400,190);
+        buttonUp = temp_1[0][0];
+        buttonDown = temp_1[0][1];
+        up = new TextureRegionDrawable(buttonUp);
+        down = new TextureRegionDrawable(buttonDown);
+        Share = new ImageButton(up,down);
+        Share.setPosition(Gdx.graphics.getWidth()/2 + 10,Gdx.graphics.getHeight() / 2 - 210);
+        Share.setSize(133,63);
+        stage.addActor(Share);
+        Share.addListener(new ClickListener() {
             @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.net.openURI("https://www.example.com");
-                return true;
+            public void clicked(InputEvent event, float x, float y) {
+                SoundManager.button.play();
+                if(!FaceBook.isVisible() && !Twitter.isVisible() && !Instagram.isVisible()){
+                    FaceBook.setVisible(true);
+                    Twitter.setVisible(true);
+                    Instagram.setVisible(true);
+                    Share.setVisible(false);
+                }
+                else if(FaceBook.isVisible() && Twitter.isVisible() && Instagram.isVisible()){
+                    FaceBook.setVisible(false);
+                    Twitter.setVisible(false);
+                    Instagram.setVisible(false);
+                }
             }
         });
-        stage.addActor(instagramPost);
-
-
 
         Gdx.input.setInputProcessor(stage);
     }
@@ -187,8 +302,8 @@ public class PostGame extends ScreenAdapter {
         stage.act();
         stage.draw();
 
-        String postGameMsg = "Artwork created by:\n"+game.getPlayerName()+" & "+game.getGameLobby().getPartnerName();
-        font.draw(game.getBatch(), postGameMsg, Gdx.graphics.getWidth()/2 - 150, Gdx.graphics.getHeight() - 20);
+        String postGameMsg = "Artwork created by: "+game.getPlayerName()+" & "+game.getGameLobby().getPartnerName();
+        smallFont.draw(game.getBatch(), postGameMsg, Gdx.graphics.getWidth()/2 - 150, Gdx.graphics.getHeight() - 60);
 
         game.getBatch().end();
 
